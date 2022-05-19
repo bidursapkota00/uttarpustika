3eb1cabbb023b6e95a10736bb65e3759223b31cf
bidursapkota00 <bidursapkota00@gmail.com>
2022-05-19 23:19:35 +0545

    animation slide

--- screens/home/home.js
+++ screens/home/home.js
@@ -1,10 +1,48 @@
-import React from 'react';
-import {View, Text} from 'react-native';
+import React, {useRef, useEffect} from 'react';
+import {
+  Animated,
+  Text,
+  View,
+  StyleSheet,
+  SafeAreaView,
+  FlatList,
+} from 'react-native';
+import UniversityItem from './universityItem';
 
-export default function Home() {
+const Home = () => {
+  const university = ['TU', 'PU', 'POU', 'KU', '+2'];
+
   return (
-    <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
-      <Text style={{color: 'black'}}>Home Screen</Text>
-    </View>
+    <SafeAreaView style={styles.container}>
+      <FlatList
+        data={university}
+        renderItem={({item, index}) => (
+          <UniversityItem index={index}>
+            <View style={styles.bg}>
+              <Text style={styles.fadingText}>{item}</Text>
+            </View>
+          </UniversityItem>
+        )}
+      />
+    </SafeAreaView>
   );
-}
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  fadingText: {
+    fontSize: 14,
+    color: 'black',
+  },
+  bg: {
+    backgroundColor: 'white',
+    padding: 30,
+    borderRadius: 10,
+    marginTop: 10,
+    marginHorizontal: 30,
+  },
+});
+
+export default Home;
