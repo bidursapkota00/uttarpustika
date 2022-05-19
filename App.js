c4060901fb16b21c2163a89415baa789892cb588
bidursapkota00 <bidursapkota00@gmail.com>
2022-05-19 10:54:01 +0545

    navigation

---
+++ App.js
@@ -0,0 +1,32 @@
+import * as React from 'react';
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import {createDrawerNavigator} from '@react-navigation/drawer';
+import Home from './screens/home/home';
+import Contact from './screens/contact/contact';
+
+const Stack = createNativeStackNavigator();
+const Drawer = createDrawerNavigator();
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Drawer.Navigator>
+        <Drawer.Screen name="Home" component={Home} />
+        <Drawer.Screen name="Contact" component={Contact} />
+      </Drawer.Navigator>
+    </NavigationContainer>
+  );
+  // return (
+  //   <NavigationContainer>
+  //     <Stack.Navigator>
+  //       <Stack.Screen
+  //         name="Home"
+  //         component={Home}
+  //         options={{title: 'University'}}
+  //       />
+  //       <Stack.Screen name="Contact" component={Contact} />
+  //     </Stack.Navigator>
+  //   </NavigationContainer>
+  // );
+}
