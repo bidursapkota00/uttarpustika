35c6fe21535f92b3bb79cbd88fad939d93f4ac9c
bidursapkota00 <bidursapkota00@gmail.com>
2022-05-27 10:51:33 +0545

    slide show

--- screens/home/home.css.js
+++ screens/home/home.css.js
@@ -1,13 +1,44 @@
 import {StyleSheet} from 'react-native';
 
 export const styles = StyleSheet.create({
+  container: {
+    position: 'relative',
+    flex: 1,
+  },
+  cont: {
+    height: '90%',
+  },
   listc: {
     display: 'flex',
     flexDirection: 'row',
     flexWrap: 'wrap',
-    marginTop: 50,
+    marginBottom: '10%',
   },
   list: {
     width: '33%',
+  },
+  slide: {
+    height: 250,
+    paddingTop: 10,
+    display: 'flex',
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  slideimg: {
+    height: '100%',
+    width: '100%',
+    borderRadius: 5,
+  },
+  ads: {
+    position: 'absolute',
+    bottom: 0,
+    left: 0,
+    width: '100%',
+    height: '10%',
+  },
+  ad: {
+    width: '100%',
+    height: '100%',
   },
 });
--- screens/home/home.js
+++ screens/home/home.js
@@ -1,49 +1,94 @@
 import React, {useRef, useEffect} from 'react';
-import {View, SafeAreaView} from 'react-native';
+import {
+  View,
+  SafeAreaView,
+  Text,
+  Image,
+  Dimensions,
+  ScrollView,
+} from 'react-native';
 import Listview from '../../components/listview/listview';
 import UniversityItem from './universityItem';
 import {styles} from './home.css';
+import Carousel, {Pagination} from 'react-native-snap-carousel';
+import {boards, slideImg} from '../../utils/data';
+
+export const SLIDER_WIDTH = Dimensions.get('window').width;
+export const ITEM_WIDTH = Math.round(SLIDER_WIDTH * 0.8);
 
 const Home = () => {
-  const boards = [
-    {
-      title: 'TU',
-      bg: '#ff4d4f',
-      icon: require('../../img/school.png'),
-      textcolor: '#5c0011',
-    },
-    {
-      title: 'PU',
-      bg: '#ff7a45',
-      icon: require('../../img/school.png'),
-      textcolor: '#610b00',
-    },
-    {
-      title: 'POU',
-      bg: '#ffa940',
-      icon: require('../../img/school.png'),
-      textcolor: '#612500',
-    },
-    {
-      title: 'KU',
-      bg: '#ffc53d',
-      icon: require('../../img/school.png'),
-      textcolor: '#613400',
-    },
-  ];
+  const carousel = useRef(null);
+  const [index, setIndex] = React.useState(0);
+
+  const _renderItem = ({item, index}) => {
+    return (
+      <View style={[styles.slide, {width: ITEM_WIDTH}]}>
+        <Image
+          style={styles.slideimg}
+          source={{
+            uri: item,
+          }}
+        />
+      </View>
+    );
+  };
 
   return (
     <SafeAreaView style={styles.container}>
-      <View style={styles.listc}>
-        {boards.map((b, i) => {
-          return (
-            <View style={styles.list} key={b.title}>
-              <UniversityItem index={i}>
-                <Listview board={b} />
-              </UniversityItem>
-            </View>
-          );
-        })}
+      <View style={styles.cont}>
+        <View>
+          <Carousel
+            ref={carousel}
+            data={slideImg}
+            renderItem={_renderItem}
+            sliderWidth={SLIDER_WIDTH}
+            itemWidth={ITEM_WIDTH}
+            autoplay
+            onBeforeSnapToItem={index => setIndex(index)}
+            autoplayDelay={2000}
+            autoplayInterval={2000}
+            loop
+            enableSnap
+            loopClonesPerSide={slideImg.length}
+            scrollEnabled
+          />
+          <Pagination
+            dotsLength={slideImg.length}
+            activeDotIndex={index}
+            carouselRef={carousel}
+            dotStyle={{
+              width: 10,
+              height: 10,
+              borderRadius: 5,
+              marginHorizontal: 0,
+              backgroundColor: 'rgba(0, 0, 0, 0.92)',
+            }}
+            inactiveDotOpacity={0.4}
+            inactiveDotScale={0.6}
+            tappableDots={true}
+          />
+        </View>
+        <ScrollView style={{height: 200}}>
+          <View style={styles.listc}>
+            {boards.map((b, i) => {
+              return (
+                <View style={styles.list} key={b.title}>
+                  <UniversityItem index={i}>
+                    <Listview board={b} />
+                  </UniversityItem>
+                </View>
+              );
+            })}
+          </View>
+        </ScrollView>
+      </View>
+      <View style={styles.ads}>
+        <Image
+          style={styles.ad}
+          source={{
+            uri: 'https://c8.alamy.com/comp/PT97M3/design-bright-banner-on-white-background-horizontal-autumn-banner-autumn-sale-30-off-vector-illustrations-for-flyers-posters-email-ads-promotional-material-PT97M3.jpg',
+          }}
+        />
       </View>
     </SafeAreaView>
   );
