4f0b8f3bbfc0744d6555dc72cd534b2f23f099da
bidursapkota00 <bidursapkota00@gmail.com>
2022-10-16 21:09:07 +0545

    registration final

--- screens/register/register.css.js
+++ screens/register/register.css.js
@@ -4,7 +4,6 @@ export const styles = StyleSheet.create({
   container: {
     padding: 25,
     paddingTop: 50,
-    flex: 1,
   },
   title: {
     textAlign: 'center',
--- screens/register/register.js
+++ screens/register/register.js
@@ -7,13 +7,25 @@ import {
   Snackbar,
   withTheme,
 } from 'react-native-paper';
-import {View} from 'react-native';
+import {View, KeyboardAvoidingView, ScrollView} from 'react-native';
 import {styles} from './register.css';
 
-function Register({theme}) {
+async function postData(url = '', data = {}) {
+  const response = await fetch(url, {
+    method: 'POST', // *GET, POST, PUT, DELETE, etc.
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(data), // body data type must match "Content-Type" header
+  });
+  return response.json(); // parses JSON response into native JavaScript objects
+}
+
+function Register() {
   const [name, setName] = useState('');
   const [email, setEmail] = useState('');
-  const [number, setNumber] = useState(0);
+  const [address, setAddress] = useState('');
+  const [number, setNumber] = useState('');
   const [password, setPassword] = useState('');
   const [confirmPassword, setConfirmPassword] = useState('');
   const [citizenship, setCitizenship] = useState('');
@@ -21,8 +33,16 @@ function Register({theme}) {
   const [visible, setVisible] = useState('');
   const onDismissSnackBar = () => setVisible('');
 
-  const onSubmit = () => {
-    if (name && email && number && password && confirmPassword && citizenship) {
+  const onSubmit = async () => {
+    if (
+      name &&
+      address &&
+      email &&
+      number &&
+      password &&
+      confirmPassword &&
+      citizenship
+    ) {
       if (password !== confirmPassword) {
         setVisible('Password and Confirm Password is not matching');
         return;
@@ -31,58 +51,80 @@ function Register({theme}) {
         setVisible('Use "-" to seperate citizenship number');
         return;
       }
-      console.log(name, email, number, password, citizenship);
+      console.log(name, address, email, number, password, citizenship);
+      const res = await postData(
+        'https://water-flow-meter.herokuapp.com/api/register',
+        {
+          name,
+          address,
+          email,
+          password,
+          number,
+          citizenship,
+        },
+      );
+      console.log(res);
     } else {
       setVisible('Data is not Complete');
     }
   };
 
   return (
-    <View style={styles.container}>
-      <Title style={styles.title}>Request for Water Service!</Title>
-      <TextInput
-        style={[styles.input]}
-        label="Full Name"
-        value={name}
-        onChangeText={text => setName(text)}
-      />
-      <TextInput
-        style={[styles.input]}
-        label="Email"
-        value={email}
-        onChangeText={text => setEmail(text)}
-      />
-      <TextInput
-        style={[styles.input]}
-        label="Phone Number"
-        value={number}
-        onChangeText={text => setNumber(text)}
-      />
-      <TextInput
-        style={styles.input}
-        label="Password"
-        secureTextEntry
-        value={password}
-        onChangeText={text => setPassword(text)}
-        // right={<TextInput.Icon name="eye" />}
-      />
-      <TextInput
-        style={styles.input}
-        label="Confirm Password"
-        secureTextEntry
-        value={confirmPassword}
-        onChangeText={text => setConfirmPassword(text)}
-        // right={<TextInput.Icon name="eye" />}
-      />
-      <TextInput
-        style={[styles.input]}
-        label="Citizenship Number"
-        value={citizenship}
-        onChangeText={text => setCitizenship(text)}
-      />
-      <Button mode="contained" onPress={onSubmit}>
-        Submit
-      </Button>
+    <KeyboardAvoidingView>
+      <ScrollView>
+        <View style={styles.container}>
+          <Title style={styles.title}>Request for Water Service!</Title>
+          <TextInput
+            style={[styles.input]}
+            label="Full Name"
+            value={name}
+            onChangeText={text => setName(text)}
+          />
+          <TextInput
+            style={[styles.input]}
+            label="Address"
+            value={address}
+            onChangeText={text => setAddress(text)}
+          />
+          <TextInput
+            style={[styles.input]}
+            label="Email"
+            value={email}
+            onChangeText={text => setEmail(text)}
+          />
+          <TextInput
+            style={[styles.input]}
+            label="Phone Number"
+            value={number}
+            onChangeText={text => setNumber(text)}
+          />
+          <TextInput
+            style={styles.input}
+            label="Password"
+            secureTextEntry
+            value={password}
+            onChangeText={text => setPassword(text)}
+            // right={<TextInput.Icon name="eye" />}
+          />
+          <TextInput
+            style={styles.input}
+            label="Confirm Password"
+            secureTextEntry
+            value={confirmPassword}
+            onChangeText={text => setConfirmPassword(text)}
+            // right={<TextInput.Icon name="eye" />}
+          />
+          <TextInput
+            style={[styles.input]}
+            label="Citizenship Number"
+            value={citizenship}
+            onChangeText={text => setCitizenship(text)}
+          />
+          <Button mode="contained" onPress={onSubmit}>
+            Submit
+          </Button>
+        </View>
+      </ScrollView>
       <View style={styles.snackbar}>
         <Snackbar
           duration={3000}
@@ -97,7 +139,7 @@ function Register({theme}) {
           {visible}
         </Snackbar>
       </View>
-    </View>
+    </KeyboardAvoidingView>
   );
 }
 
